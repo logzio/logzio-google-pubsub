f0468f667a614bfdd91a77f3824972bac56a9903
AntonKolomiiets <anton.kolomiiets@logz.io>
2022-11-02 16:01:23 +0200

    update cloud function

--- cloud_function_go/function.go
+++ cloud_function_go/function.go
@@ -2,13 +2,38 @@
 package p
 
 import (
+	"bytes"
 	"encoding/base64"
 	"encoding/json"
 	"fmt"
 	"net/http"
-
-	"github.com/logzio/logzio-go"
+	"time"
 )
+
+func shouldRetry(statusCode int) bool {
+	retry := true
+	switch statusCode {
+	case http.StatusBadRequest:
+		fmt.Printf("Got HTTP %d bad request, skip retry\n", statusCode)
+		retry = false
+	case http.StatusNotFound:
+		fmt.Printf("Got HTTP %d not found, skip retry\n", statusCode)
+		retry = false
+	case http.StatusUnauthorized:
+		fmt.Printf("Got HTTP %d unauthorized, skip retry\n", statusCode)
+		retry = false
+	case http.StatusForbidden:
+		fmt.Printf("Got HTTP %d forbidden, skip retry\n", statusCode)
+		retry = false
+	case http.StatusOK:
+		retry = false
+	}
+	return retry
+}
+
+func argumentValidation(token string, typeLog string, listener string) bool {
+
+}
 
 func LogzioHandler(w http.ResponseWriter, r *http.Request) {
 	type Data struct {
@@ -21,26 +46,52 @@ func LogzioHandler(w http.ResponseWriter, r *http.Request) {
 	token := r.URL.Query().Get("token")
 	typeLog := r.URL.Query().Get("type")
 	listener := r.URL.Query().Get("listener")
-	url := fmt.Sprintf("https://%s:8071", listener)
-	tokenAddType := fmt.Sprintf("%s&type=%s", token, typeLog)
+
+	if token != "" {
+		fmt.Printf("Logzio token must be provided")
+		return
+	}
+	if listener != "" {
+		fmt.Printf("Logzio listener must be provided")
+		return
+	}
+	if typeLog != "" {
+		fmt.Printf("Set default log type, `pubsub`")
+		typeLog = "pubsub"
+	}
+
+	url := fmt.Sprintf("https://%s:8071/?token=%s&type=%s", listener, token, typeLog)
 	err := json.NewDecoder(r.Body).Decode(&d)
 	if err != nil {
-		return nil, fmt.Errorf("Can't decode request's body with log message: %w", err)
+		fmt.Printf("Can't decode request's body with log message: %w", err)
+		return
 	}
 
 	rawDecodedText, err := base64.StdEncoding.DecodeString(d.Message.Data)
 	if err != nil {
-		return nil, fmt.Errorf("Message log can't be parsed: %w", err)
+		fmt.Printf("Message log can't be parsed: %w", err)
+		return
 	}
 
-	l, err := logzio.New(tokenAddType, logzio.SetUrl(url)) // token is required
-	if err != nil {
-		return nil, fmt.Errorf("Logz.io connection failed: %w", err)
+	backOff := time.Second * 2
+	sendRetries := 4
+	toBackOff := false
+	for attempt := 0; attempt < sendRetries; attempt++ {
+		if toBackOff {
+			fmt.Printf("Failed to send logs, trying again in %v\n", backOff)
+			time.Sleep(backOff)
+			backOff *= 2
+		}
+		resp, err := http.Post(url, "application/json",
+			bytes.NewBuffer(rawDecodedText))
+		if err != nil {
+			fmt.Printf("Can't send data to logz.io, reason is: %w", err)
+			return
+		}
+		if shouldRetry(resp.StatusCode) {
+			toBackOff = true
+		} else {
+			break
+		}
 	}
-	err = l.Send([]byte(rawDecodedText))
-	if err != nil {
-		return nil, fmt.Errorf("Can't send log data to logz.io: %w", err)
-	}
-	l.Stop()
-	return
 }
